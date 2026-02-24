d92d231243017de2f16eea89e38ef654f9865571
krisishikkha <krisishikkha@gmail.com>
2026-02-24 16:52:23 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -15,50 +15,50 @@ const EXAM_STATUS = {
 
   "exam-3": {
     title: "HSC 1st Paper 2nd Chapter",
-    visible: true,  // ✅ নতুন
+    visible: false,  // ✅ নতুন
     status: "live",
     codes: ["exam3code"]
   },
   "exam-4": {
     title: "HSC 1st Paper 3rd Chapter",
-    visible: true,  // ✅ নতুন
+    visible: false,  // ✅ নতুন
     status: "live",
     codes: ["exam3code"]
       },
   "exam-5": {
     title: "HSC 1st Paper 4rth Chapter",
-    visible: true,  // ✅ নতুন
+    visible: false,  // ✅ নতুন
     status: "live",
     codes: ["exam2code"]
     },
     "exam-6": {
     title: "HSC 1st Paper 5th Chapter",
-    visible: true,  // ✅ নতুন
+    visible: false,  // ✅ নতুন
     status: "live",
     codes: ["exam2code"]
     },
     "exam-7": {
     title: "HSC 1st Paper 6th Chapter",
-    visible: true,  // ✅ নতুন
+    visible: false,  // ✅ নতুন
     status: "live",
     codes: ["exam2code"]
     },
     "exam-8": {
     title: "HSC 2nd Paper 1st Chapter",
-    visible: true,  // ✅ নতুন
-    status: "locked",
+    visible: false,  // ✅ নতুন
+    status: "live",
     codes: ["exam2code"]
     },
     "exam-9": {
     title: "HSC 2nd Paper 2nd Chapter",
-    visible: true,  // ✅ নতুন
-    status: "locked",
+    visible: false,  // ✅ নতুন
+    status: "live",
     codes: ["exam2code"]
     },
     "exam-10": {
     title: "HSC 2nd Paper 3rd Chapter",
-    visible: true,  // ✅ নতুন
-    status: "locked",
+    visible: false,  // ✅ নতুন
+    status: "live",
     codes: ["exam2code"]
     }
 };
