213367575a600cb8ed61c78b45929c136e3cbf6e
krisishikkha <krisishikkha@gmail.com>
2026-03-12 11:23:38 +0600

    Change exam-3 status from 'live' to 'locked'

--- exam-status.js
+++ exam-status.js
@@ -16,7 +16,7 @@ const EXAM_STATUS = {
   "exam-3": {
     title: "HSC 1st Paper 2nd Chapter",
     visible: false,  // ✅ নতুন
-    status: "live",
+    status: "locked",
     codes: ["exam31"]
   },
   "exam-4": {
