97e89f62c5e8da1adc2fe06f95c1ecebde13cb69
krisishikkha <krisishikkha@gmail.com>
2026-03-13 23:44:49 +0600

    Update users.js

--- bcs/data/users.js
+++ bcs/data/users.js
@@ -1,1 +1,26 @@
-
+const BCS_USERS = [
+  {
+    username: "student01",
+    password: "bcs123",
+    access: "active",
+    expires: "2026-12-31"
+  },
+  {
+    username: "student02",
+    password: "krishi456",
+    access: "active",
+    expires: "2026-10-15"
+  },
+  {
+    username: "demoexpired",
+    password: "expired123",
+    access: "active",
+    expires: "2025-01-01"
+  },
+  {
+    username: "blockeduser",
+    password: "blocked123",
+    access: "inactive",
+    expires: "2026-12-31"
+  }
+];
