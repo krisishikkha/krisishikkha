031131d0e3054a3d7875c780557fd40541dd85b3
krisishikkha <krisishikkha@gmail.com>
2026-03-14 19:04:58 +0600

    Update users.js

--- research/data/users.js
+++ research/data/users.js
@@ -1,1 +1,26 @@
-
+const RESEARCH_USERS = [
+  {
+    username: "research01",
+    password: "demo123",
+    access: "active",
+    expires: "2026-12-31"
+  },
+  {
+    username: "research02",
+    password: "notes456",
+    access: "active",
+    expires: "2026-10-15"
+  },
+  {
+    username: "expireduser",
+    password: "expired123",
+    access: "active",
+    expires: "2025-01-01"
+  },
+  {
+    username: "blockeduser",
+    password: "blocked123",
+    access: "inactive",
+    expires: "2026-12-31"
+  }
+];
