bdf59de8cd029b466cc650f597359dd8bf5156e8
krisishikkha <krisishikkha@gmail.com>
2026-03-14 18:55:53 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -21,9 +21,9 @@ const EXAM_STATUS = {
   },
   "exam-4": {
     title: "HSC 1st Paper 3rd Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
-    codes: ["exam39"]
+    codes: ["exam349"]
       },
   "exam-5": {
     title: "HSC 1st Paper 4rth Chapter",
