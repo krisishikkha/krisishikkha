386ca7a44f8fca119f983b77e250573811b1daa8
krisishikkha <krisishikkha@gmail.com>
2026-03-15 22:39:58 +0600

    Update config.js

--- bcs/config.js
+++ bcs/config.js
@@ -1,15 +1,8 @@
 const BCS_CONFIG = {
-
   whatsappNumber: "8801516013089",
-
   whatsappMessage: "Hello, I want access to premium BCS study materials.",
-
-  lockedTitle: "Premium Content Locked",
-
-  lockedText: "This content is available for premium BCS users only.",
-
+  lockedTitle: "Premium Content",
+  lockedText: "This content is available for premium users only.",
   expiredText: "Your access is expired or inactive. Please contact support.",
-
-  loginText: "This is a premium resource. Please contact us on WhatsApp to get login access and unlock the content."
-
+  loginText:"This is a premium resource. Please contact us on WhatsApp to get login access and unlock the content."
 };
