7224b7fd6a97df669689fbeed810ccb5b9c4c4c4
krisishikkha <krisishikkha@gmail.com>
2026-03-17 22:53:48 +0600

    Update article-protect.js

--- diploma/assets/js/article-protect.js
+++ diploma/assets/js/article-protect.js
@@ -1,1 +1,7 @@
+document.addEventListener("contextmenu", e => e.preventDefault());
 
+document.addEventListener("keydown", function(e) {
+  if (e.ctrlKey && ["c", "u", "s"].includes(e.key.toLowerCase())) {
+    e.preventDefault();
+  }
+});
