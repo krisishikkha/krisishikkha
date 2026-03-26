d2de75a46e37c07bd1d298713ee2a3eb6ab3e802
krisishikkha <krisishikkha@gmail.com>
2026-03-26 20:28:40 +0600

    Create users.js

---
+++ diploma/data/users.js
@@ -0,0 +1,26 @@
+const AGRICULTURE_USERS = [
+  {
+    username: "agri01",
+    password: "field123",
+    access: "active",
+    expires: "2026-12-31"
+  },
+  {
+    username: "agri02",
+    password: "soil456",
+    access: "active",
+    expires: "2026-10-15"
+  },
+  {
+    username: "expiredagri",
+    password: "expired789",
+    access: "active",
+    expires: "2025-01-01"
+  },
+  {
+    username: "blockedagri",
+    password: "blocked456",
+    access: "inactive",
+    expires: "2026-12-31"
+  }
+];
