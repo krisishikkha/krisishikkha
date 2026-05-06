4fafc1c9e3d8f49ba8a60bd1e95129a22da4d907
krisishikkha <krisishikkha@gmail.com>
2026-05-06 10:49:51 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -104,14 +104,14 @@ const EXAM_STATUS = {
     codes: ["exam334"]
     },
   "exam-18": {
-    title: "SSC 6th Chapter🔴",
-    visible: true,  // ✅ নতুন
+    title: "SSC 6th Chapter",
+    visible: false,  // ✅ নতুন
     status: "live",
     codes: ["exam3355"]
     },
   "exam-19": {
-    title: "SSC 7th Chapter",
-    visible: false,  // ✅ নতুন
+    title: "SSC 7th Chapter🔴",
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam333"]
     }
