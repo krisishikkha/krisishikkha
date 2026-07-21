183239ecf523f2156cc77ad897fddf3ae4bd64d3
krisishikkha <krisishikkha@gmail.com>
2026-07-21 14:45:07 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -15,97 +15,97 @@ const EXAM_STATUS = {
 
   "exam-3": {
     title: "HSC 1st Paper 2nd Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam321"]
   },
   "exam-4": {
     title: "HSC 1st Paper 3rd Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam3490"]
       },
   "exam-5": {
     title: "HSC 1st Paper 4rth Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam6262"]
     },
     "exam-6": {
     title: "HSC 1st Paper 5th Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam2668"]
     },
     "exam-7": {
     title: "HSC 1st Paper 6th Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam246"]
     },
     "exam-8": {
     title: "HSC 2nd Paper 1st Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam2"]
     },
     "exam-9": {
     title: "HSC 2nd Paper 2nd Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam222"]
     },
     "exam-10": {
     title: "HSC 2nd Paper 3rd Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam3333"]
     },
   "exam-11": {
     title: "HSC 2nd Paper 4rth Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam1122"]
     },
   "exam-12": {
     title: "HSC 2nd Paper 5th Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam2244"]
     },
   "exam-13": {
     title: "SSC 1st Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam4443"]
     },
   "exam-14": {
     title: "SSC 2nd Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam3377"]
     },
   "exam-15": {
     title: "SSC 3rd Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam3333"]
     },
   "exam-16": {
     title: "SSC 4rth Chapter",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam4333"]
     },
   "exam-17": {
     title: "SSC 5th Chapter🔴",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam3344"]
     },
   "exam-18": {
     title: "SSC 6th Chapter🔴",
-    visible: false,  // ✅ নতুন
+    visible: true,  // ✅ নতুন
     status: "live",
     codes: ["exam3355"]
     },
