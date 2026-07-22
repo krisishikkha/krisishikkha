092a7d69763f5fde877b7afa9493180fb63362b1
krisishikkha <krisishikkha@gmail.com>
2026-07-22 17:54:47 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -130,5 +130,152 @@ const EXAM_STATUS = {
     visible: false,
     status: "live",
     codes: ["exam1111"]
+  },
+
+  "exam-20": {
+    title: "HSC 1st Paper 1st Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-21": {
+    title: "HSC 1st Paper 2nd Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-22": {
+    title: "HSC 1st Paper 3rd Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-23": {
+    title: "HSC 1st Paper 4rth Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-24": {
+    title: "HSC 1st Paper 5th Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-25": {
+    title: "HSC 1st Paper 6th Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-26": {
+    title: "HSC 2nd Paper 1st Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-27": {
+    title: "HSC 2nd Paper 2nd Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-28": {
+    title: "HSC 2nd Paper 3rd Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-29": {
+    title: "HSC 2nd Paper 4rth Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-30": {
+    title: "HSC 2nd Paper 5th Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-31": {
+    title: "SSC 1st Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-32": {
+    title: "SSC 2nd Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-33": {
+    title: "SSC 3rd Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-34": {
+    title: "SSC 4rth Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-35": {
+    title: "SSC 5th Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-36": {
+    title: "SSC 6th Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-37": {
+    title: "SSC 7nth Chapter",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-38": {
+    title: "None",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-39": {
+    title: "None",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
+  },
+
+  "exam-40": {
+    title: "None",
+    visible: true,
+    status: "live",
+    codes: ["exam2222"]
   }
 };
