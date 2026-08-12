02e8d66f41a9399062c621f4d291b4724bdd2f84
krisishikkha <krisishikkha@gmail.com>
2026-08-12 15:55:38 +0600

    Update exam-status.js

--- exam-status.js
+++ exam-status.js
@@ -38,112 +38,111 @@ const EXAM_STATUS = {
     endDate: "2026-09-25T11:00:00+06:00",
     codes: ["1111"]
   },
-
-  "exam-2": {
+"exam-2": {
     title: "HSC 1st Paper 1st Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-3": {
     title: "HSC 1st Paper 2nd Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-4": {
     title: "HSC 1st Paper 3rd Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-5": {
     title: "HSC 1st Paper 4rth Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-6": {
     title: "HSC 1st Paper 5th Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-7": {
     title: "HSC 1st Paper 6th Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-8": {
     title: "HSC 2nd Paper 1st Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-9": {
     title: "HSC 2nd Paper 2nd Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-10": {
     title: "HSC 2nd Paper 3rd Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-11": {
     title: "HSC 2nd Paper 4rth Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-12": {
     title: "HSC 2nd Paper 5th Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-13": {
     title: "SSC 1st Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-14": {
     title: "SSC 2nd Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-15": {
     title: "SSC 3rd Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-16": {
     title: "SSC 4rth Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-17": {
     title: "SSC 5th Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-18": {
     title: "SSC 6th Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
   "exam-19": {
     title: "SSC 7th Chapter",
-    category: "archived",
+    category: "draft",
     codes: ["exam1111"]
   },
 
